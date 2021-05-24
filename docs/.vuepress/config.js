f0ee2d370aa385ca87ec10577ef979ac96e8a7d3
abiscuit <account@abiscuit.io>
2021-05-24 11:34:02 +0800

    feat: #61 homepage products links to latest versions

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -76,6 +76,7 @@ module.exports = {
     nav: [
       {
         text: 'Rsyslog',
+        image: '/images/products/rsyslog.png',
         items: [
           {
             text: '1.3.0',
@@ -85,6 +86,7 @@ module.exports = {
       },
       {
         text: 'SFTP',
+        image: '/images/products/sftp.png',
         items: [
           {
             text: '1.0.1',
@@ -98,6 +100,7 @@ module.exports = {
       },
       {
         text: 'VPN',
+        image: '/images/products/vpn.png',
         items: [
           {
             text: '1.1.0',
@@ -107,6 +110,7 @@ module.exports = {
       },
       {
         text: 'Samba',
+        image: '/images/products/samba.png',
         items: [
           {
             text: '1.2.0',
